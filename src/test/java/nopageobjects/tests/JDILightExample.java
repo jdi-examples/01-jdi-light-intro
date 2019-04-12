a592c65126a748cc8b7555fed846bf88a5c29324
RomanIovlev <Roman_Iovlev@epam.com>
2019-04-12 16:16:49 +0300

    correct simple test

--- src/test/java/nopageobjects/tests/JDILightExample.java
+++ src/test/java/nopageobjects/tests/JDILightExample.java
@@ -3,20 +3,28 @@ package nopageobjects.tests;
 import com.epam.jdi.light.elements.composite.WebPage;
 import org.testng.annotations.Test;
 
+import static com.epam.jdi.light.elements.composite.WebPage.*;
+import static com.epam.jdi.light.ui.html.HtmlFactory.$;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+
 public class JDILightExample {
     @Test
-    public void openPage() {
-        WebPage.openUrl("http://www.baeldung.com/");
+    public void openAboutPage() {
+        openUrl("http://www.baeldung.com/");
+        $(".menu-about a").click();
+        $("//h3[contains(.,'About Baeldung')]").click();
+        assertThat(getUrl(), is("https://www.baeldung.com/about/"));
     }
 
     @Test
     public void actionsWithPage() {
-        WebPage.openUrl("http://www.baeldung.com/");
-        WebPage.getUrl();
-        WebPage.getTitle();
-        WebPage.back();
-        WebPage.forward();
-        WebPage.getHtml();
-        WebPage.refresh();
+        openUrl("http://www.baeldung.com/");
+        getUrl();
+        getTitle();
+        back();
+        forward();
+        getHtml();
+        refresh();
     }
 }
